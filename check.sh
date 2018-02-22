685b01ac7338caabb9c50dd903b253d3ab2956bd
Modulus <thejohnskauge@gmail.com>
2018-02-22 21:49:01 +0100

    Updated

--- check.sh
+++ check.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+if [ -z  "$1" ]
+then
+    echo "You have to enter the ip address to check, with port"
+    echo "Usage example: check http://192.168.99.109:32087"
+    echo "\n"
+else
+    echo "Listening on ip: $1\n"
+fi
+
 while true
-do
-    curl http://192.168.99.100:32082
+do  
+    output= curl "$1"
+    echo "$output"
     sleep 1
 done
---
+++ watch.sh
@@ -0,0 +1,6 @@
+while true
+do
+    kubectl get pods
+    echo "\n"
+    sleep 2
+done
